e7c75287d31291592435a594af8fd72616e82693
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-12-22 14:01:13 +0000

    Reformat code

--- bin/format_challenge.py
+++ bin/format_challenge.py
@@ -26,28 +26,10 @@ This script will apply markdown blockquotes ">" as required
 import sys
 import re
 
-if len(sys.argv) == 1:
-    print ("Using stdin")
-    infile = sys.stdin
-    outfile = sys.stdout
-elif len(sys.argv) == 3:
-    print ("Reading files")
-    infile = open(sys.argv[1])
-    outfile = open(sys.argv[2], "w")
-else:
-    print("Must supply input *and* output files, or use STDIN/OUT")
-
-# Read in file
-lesson = infile.readlines()
-
-lesson = lesson[::-1]
-
 
 def markblock(inlist, blocktype,  startmarker=r'^##'):
     """ "> "ise a block of text from startmarker to endmarker """
     outlist = inlist[:] 
-    # If we match the blocktype
-    # We need to check if the next line is already "> "ed
     linessinceblock = 0
     inblock = False
     for i,l in enumerate(outlist):
@@ -68,12 +50,33 @@ def markblock(inlist, blocktype,  startmarker=r'^##'):
     return outlist
 
         
+
+if len(sys.argv) == 1:
+    print ("Using stdin")
+    infile = sys.stdin
+    outfile = sys.stdout
+elif len(sys.argv) == 3:
+    print ("Reading files")
+    infile = open(sys.argv[1])
+    outfile = open(sys.argv[2], "w")
+else:
+    print("Must supply input *and* output files, or use STDIN/OUT")
+
+# Read in file
+lesson = infile.readlines()
+
+# Reverse lines
+lesson = lesson[::-1]
+
+
 # Run function on {: .solution}
 lesson = markblock(lesson, r"^\{: \.solution\}")
 # Run function on {: .challenge}
 lesson = markblock(lesson, r"^\{: \.challenge\}")
 
+# Put lines back in the correct order
 lesson = lesson[::-1]
+
 # Output file
 outfile.writelines(lesson)
 
